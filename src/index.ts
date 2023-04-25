89418443199edc8fa46f975eb3f03792145eb5cb
M-C-Ribeiro <87188847+M-C-Ribeiro@users.noreply.github.com>
2023-04-25 20:39:05 -0300

    commit 25 de abril

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-let  texto: string;
+import express from 'express';
+import mongoose from 'mongoose';
+import { router } from './router'; 
 
-texto = 'Hello World';
+mongoose.connect("mongodb://localhost:27017")
+	.then (() => {
+		const app = express();
+		const port = 3000;
 
-console.log(texto);
+		app.use(express.json());
+		app.use(router);
+		//teclado de emotion tecla win+.
+		app.listen(port, () => {
+			console.log(`🚗Server is runing on http://localhost:${port}`);
+		});
+	})
+	.catch(() => console.log("Erro ao conectar ao mongoDB"))
+		
